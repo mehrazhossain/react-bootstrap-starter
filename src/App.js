a1210b0beb9d955aa15228d3738320cb0a95a227
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-10-28 12:47:48 +0600

    added React bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    <div>
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+const Greet = () => {
+  return (
+    <div>
+      <p>Hello, world!</p>
+      <Button variant="success">Success</Button>
+    </div>
+  );
+};
+
+export default Greet;
